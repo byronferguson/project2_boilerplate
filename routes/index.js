450643e6e622826311fc7459952a4ade756cbf7e
Byron Ferguson <ferguson.bs@gmail.com>
2020-12-17 19:09:23 -0600

    adding error

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
-const htmlRoutes = require('./html');
+var router = require('express').Router();
+var apiRoutes = require('./api');
+var htmlRoutes = require('./html');
 
 router.use('/api', apiRoutes);
 router.use('/', htmlRoutes);
